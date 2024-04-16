ad45e6253f71fd69bb9889ef97dab5489b469d6e
chenyj <chenyj@chenyj.lan>
2024-04-16 08:58:55 -0500

    initial project

---
+++ src/HelloWorld.jsx
@@ -0,0 +1,9 @@
+
+function HelloWorld() {
+    return(
+        <>
+        <h1 className="text-center">Hello World!</h1>
+        </>
+    )
+}
+export default HelloWorld
---
+++ src/components/EmployeeComponent.jsx
@@ -0,0 +1,145 @@
+import React, { useEffect, useState} from 'react'
+import {AddEmployee as createEmployee, getEmployee, updateEmployee} from '../services/EmployeeService'
+import { useNavigate, useParams } from 'react-router-dom'
+
+const EmployeeComponent = () => {
+    const [firstName, setFirstName] = useState('')
+    const [lastName, setLastName] = useState('')
+    const [email, setEmail] = useState('')
+
+    const [errors, setErrors] = useState({
+        firstName: "",
+        lastName: "", 
+        email: ""
+    })
+    const navigator = useNavigate()
+    const {id} = useParams()
+
+    useEffect(() => {
+        if(id){
+            getEmployee(id).then((response) => {
+                setFirstName(response.data.firstName)
+                setLastName(response.data.lastName);
+                setEmail(response.data.email);
+            }).catch((error) => {
+                console.error(error);
+            }
+        )}
+    }, [id])
+
+
+    function validateForm() {
+        let valid = true;
+        const errorsCopy = {... errors};
+        if(firstName.trim()) {
+            errorsCopy.firstName = '';
+        } else {
+            errorsCopy.firstName = 'First Name is required';
+            valid = false;
+        }
+        if(lastName.trim()) {
+            errorsCopy.lastName = '';
+        } else {
+            errorsCopy.lastName = 'Last Name is required';
+            valid = false;
+        }
+        if(email.trim()) {
+            errorsCopy.email = '';
+        } else {
+            errorsCopy.email = 'Email Address is required';
+            valid = false
+        }
+        setErrors(errorsCopy);
+        return valid;
+    }
+
+    const saveOrUpdateEmployee = (e) => {
+        e.preventDefault();
+        if(validateForm()) {
+            const employee = {firstName, lastName, email}
+            console.log(employee)
+            if(id) {
+                updateEmployee(id, employee).then((response) => {
+                    console.log(response.data)
+                    navigator("/employees")
+                }).catch(error => console.error(error))
+            } else {
+                createEmployee(employee).then((response) => {
+                    console.log(response.data)
+                    navigator('/employees')
+                }).catch(error => console.error(error))
+            }
+        }
+    }
+    function pageTitle() {
+        if(id){
+            return (<h2 className="card-title text-center">Update Employee</h2>)
+        }
+        else {
+            return (<h2 className="card-title text-center">Add Employee</h2>)
+        }
+    }
+  return (
+    <div className="container">
+        <br></br><br></br>
+        <div className="row">
+            <div className="card col-md-6 offset-md-3">
+            {/* <h2 className="text-center">Add Employee</h2> */
+                pageTitle()
+            }
+            <div className="card-body col-md-6 offset-md-3">
+                <form>
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>Employee First Name: </label>
+                        <input
+                            type="text"
+                            placeholder="Enter Employee First Name"
+                            name="firstName"
+                            value={firstName}
+                            // className="form-control"
+                            // You use ${} in string templates like const k = `my name is ${name}` you 
+                            // use {} whenever you want to put some Javas code in your 
+                            // JSX code like <div attr={name}>{name}</div>
+                            className = {`form-control ${ errors.firstName ? 'is-invalid':''}`}
+                            onChange={ (e) => setFirstName(e.target.value) } 
+                        >                   
+                        </input> 
+                        {errors.firstName && <div className='invalid-feedback'>{ errors.firstName }</div> }
+                    </div>
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>Employee Last Name: </label>
+                        <input
+                            type="text"
+                            placeholder="Enter Employee Last Name"
+                            name="lastName"
+                            value={lastName}
+                            className={`form-control ${errors.lastName ? 'is-invalid':''}`}
+                            onChange={(e) => setLastName(e.target.value)}
+                            >                     
+                        </input>
+                        {errors.lastName && <div className='invalid-feedback'>{ errors.lastName }</div>}
+                    </div>
+                    <div className='form-group mb-2'>
+                        <label className='form-label'>Employee Email: </label>
+                        <input
+                            type="text"
+                            placeholder="Enter Employee Email:"
+                            name="email"
+                            value={email}
+                            className={`form-control ${errors.email ? 'is-invalid': ''}`}
+                            onChange={(e) => setEmail(e.target.value)}
+                            >                      
+                        </input> 
+                        { errors.email && <div className='invalid-feedback'>{ errors.email}</div> }
+                    </div>
+                    <button className="btn btn-success" onClick={saveOrUpdateEmployee}>Submit</button>
+                </form>
+                </div>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default EmployeeComponent
